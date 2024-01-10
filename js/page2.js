05108d07640c03af8c0f593fdbdcd7899de4b5c0
Shweta Bapna <jainsiddhi853@gmail.com>
2024-01-10 17:55:55 +0530

    add files

---
+++ js/page2.js
@@ -0,0 +1,16 @@
+function customize(productType) {
+    const modal = document.getElementById('customization-modal');
+    // Show the customization modal
+    modal.style.display = 'block';
+    // You can add customization options dynamically based on the product type
+}
+
+function closeModal() {
+    const modal = document.getElementById('customization-modal');
+    // Close the customization modal
+    modal.style.display = 'none';
+}
+
+function addToCart() {
+    // Implement the logic to add the customized product to the shopping cart
+}
